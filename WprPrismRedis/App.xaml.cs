30a9ba570f5cdf8edeb74aa0db17099505c08396
Aleksey <aleksey.dantist@gmail.com>
2022-09-12 20:25:01 +0600

    Добавьте файлы проекта.

---
+++ WprPrismRedis/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Ioc;
+
+namespace WprPrismRedis
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App 
+    {
+
+        protected override Window CreateShell()
+        {
+            var shell = Container.Resolve<Main>();
+            return shell;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
