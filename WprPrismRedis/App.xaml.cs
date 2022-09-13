3292b1a43f300bcaf320d6065055ab9225b62d72
Aleksey <aleksey.dantist@gmail.com>
2022-09-13 08:49:08 +0600

    feat: add folder and some lines of code

--- WprPrismRedis/App.xaml.cs
+++ WprPrismRedis/App.xaml.cs
@@ -5,25 +5,32 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Prism.DryIoc;
 using Prism.Ioc;
+using Prism.Modularity;
 
 namespace WprPrismRedis
 {
     /// <summary>
     /// Логика взаимодействия для App.xaml
     /// </summary>
-    public partial class App 
+    public partial class App : PrismApplication
     {
 
         protected override Window CreateShell()
         {
-            var shell = Container.Resolve<Main>();
-            return shell;
+            return Container.Resolve<Main>();
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
-            
+            //containerRegistry.RegisterForNavigation<TabView>();
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            base.ConfigureModuleCatalog(moduleCatalog);
+            moduleCatalog.AddModule<MainModule>();
         }
     }
 }
